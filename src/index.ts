fcb47ec4798d43b20d7038b8bda733c36d5b6f02
meliaronson <melinaaronson@gmail.com>
2025-09-09 13:36:47 -0300

    CRUD character y CRUD characterClass

--- src/index.ts
+++ src/index.ts
@@ -2,55 +2,83 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from "mysql2/promise";
 import { eq } from 'drizzle-orm';
-//import { usersTable } from './db/schema';
+import { character, characterClass } from './db/schema';
 
-
-const connection = await mysql.createConnection({
+/*const connection = await mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",   // tu password
-  database: "mysql",
-});
+  database: "dsw_poc",
+});*/                 //esto lo comento porque tenemos el .env con el link al DATABASE_URL
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL no está definido en el archivo .env");
+}
+const connection = await mysql.createConnection(process.env.DATABASE_URL)
 
 export const db = drizzle(connection);
 
-
 //const db = drizzle(process.env.DATABASE_URL!);
 
-
-/*
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
+  //----------CRUD characterClass----------
+
+  // Create
+  const newClass: typeof characterClass.$inferInsert = {
+    name: 'Lord Sith',
+    descripcion: 'A wielder of the force who has embraced the dark side',
   };
+  await db.insert(characterClass).values(newClass);
+  console.log('New character class created!')
 
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
+  // Read
+  const classes = await db.select().from(characterClass);
+  console.log('Getting all character classes from the database: ', classes)
 
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-  
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  
-
+  // Update
   await db
-    .update(usersTable)
+    .update(characterClass)
     .set({
-      age: 31,
+      descripcion: 'Wielder of the force who has embraced the dark side',
     })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
+    .where(eq(characterClass.name, 'Lord Sith'));
+  console.log('Character class info updated!')
 
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+  // Delete     (COMENTAR PARA PODER EJECUTAR LA CRUD DE CHARACTER)
+  await db.delete(characterClass).where(eq(characterClass.name, 'Lord Sith'));
+  console.log('Character class deleted!')
+
+
+  //----------CRUD character----------
+
+  // Create
+  const newCharacter: typeof character.$inferInsert = {
+    name: 'Darth Vader',
+    level: 10,
+    hp: 100,
+    attack: 25,
+    class: 1,
+  };
+  await db.insert(character).values(newCharacter);
+  console.log('New character created!')
+
+  // Read
+  const characters = await db.select().from(character);
+  console.log('Getting all characters from the database: ', characters)
+
+  // Update
+  await db
+    .update(character)
+    .set({
+      level: 11,
+    })
+    .where(eq(character.name, 'Darth Vader'));
+  console.log('Character info updated!')
+
+  // Delete
+  await db.delete(character).where(eq(character.name, 'Darth Vader'));
+  console.log('Character deleted!')
 }
 
-main();*/
+main();
