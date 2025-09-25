e036151ac036fd6372f42ea98390a0742f13a4a4
Alvaro Tomasino <atomasino08@gmail.com>
2025-09-25 15:48:47 -0300

    prueba terminada para video

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/mysql2'
+import { eq } from 'drizzle-orm'
 import { seed, reset } from 'drizzle-seed'
 import * as schema from './db/schema.js'
+import {
+  character,
+  characterClass,
+  characterToItems,
+  item,
+} from './db/schema.js'
 
-const db = drizzle(process.env.DATABASE_URL!)
+const db = drizzle(process.env.DATABASE_URL!, { schema, mode: 'default' })
 await reset(db, schema)
 
 // seeding
 await seed(db, schema) // por default inserta 10 registros de c/tabla
 
 // Read
-
-/*
-const characters = await db.query.character.findMany({
-  columns: {
-    name: true,
-    level: true,
-    hp: true,
-    attack: true,
-  },
-  with: { classC: true, items: { with: { item: true } } },
-})
-*/
-
 const result = await db
-  .select()
+  .select({
+    id: character.id,
+    name: character.name,
+    className: characterClass.name,
+    items: item.name,
+  })
   .from(character)
-  .console.dir(result, { depth: null, colors: true })
+  .innerJoin(characterClass, eq(character.classId, characterClass.id))
+  .innerJoin(
+    schema.characterToItems,
+    eq(character.id, characterToItems.characterId)
+  )
+  .innerJoin(item, eq(characterToItems.itemId, item.id))
+
+console.clear()
+console.dir(result, { depth: null, colors: true })
